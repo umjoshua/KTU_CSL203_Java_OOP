410b2e43797ea177cbe350bd458a70afe8fae789
umjoshua <umjoshua@gmail.com>
2021-02-21 23:38:08 +0530

    KTU CSL203 Solutions

--- 5_abstract.java
+++ 5_abstract.java
@@ -3,7 +3,7 @@ named Shape that contains an empty method named numberOfSides( ).
 Provide three classes named Rectangle, Triangle and Hexagon such that each one 
 of the classes extends the class Shape. Each one of the classes contains only the 
 method numberOfSides( ) that shows the number of sides in the given geometrical structures.*/
-abstract class 5_abstract{
+abstract class Shape{
     abstract void numberOfSides();
      }
    class Rectangle extends Shape {
@@ -21,7 +21,7 @@ abstract class 5_abstract{
        System.out.println("Hexagon has 6 sides");
      }
    }
-   class Experiment4{
+   class 5_abstract{
      public static void main(String[] args){
          Rectangle rect = new Rectangle();
          Triangle tri = new Triangle();
